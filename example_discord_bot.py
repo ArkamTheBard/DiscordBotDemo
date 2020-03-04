ed9eeaa7f40d8b1dd60daf2ab5426b2de8916df6
Yardley Gutierrez <9100062+ArkamTheBard@users.noreply.github.com>
2020-03-04 04:52:13 -0600

    Update example_discord_bot.py
    Figured out command_prefix and we can now use client.command() but it has weird console output for commands done in original way, may have to rewrite them.

--- example_discord_bot.py
+++ example_discord_bot.py
@@ -1,4 +1,6 @@
 import discord,os,requests,youtube_dl,random
+from discord.ext.commands import Bot
+from discord import User
 from discord.utils import get
 from discord.ext import commands
 
@@ -9,7 +11,9 @@ COMMANDS = '\n+c - Prints a list of currently available commands\n' + '+g - Gree
 #https://www.youtube.com/watch?v=GgnClrx8N2k
 class MyClient(discord.Client):
     #When the program is executed, the bot will sign in and this method will execute
-    
+
+    client = Bot(command_prefix = '+') #figured this out command_prefix now defined with '+'
+    #@client.command() #TODO this kinda breaks it rn (weird console output) will need it for other commands
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
     #Tells bot to join channel where the command originated from
@@ -31,7 +35,7 @@ class MyClient(discord.Client):
         if authorM == client.user:
             return
         else:
-            if message.content.startswith('+c'):
+            if message.content.startswith('+c'): #TODO we may have to rewrite these to use @client.command format so prefixes actually work
                 await message.channel.send(COMMANDS)
             elif message.content.startswith('+g'):
                 GREETINGS = [f'DEE-BOT GREETING RAY ACTIVATE!!! HELLO {authorM}-SAMA!!!!', f'Hello {authorM}, Allonsy!',f'Ey yo what up with it {authorM}',f'Ascend through violence {authorM}! You can do it!',f'Ohaiyo good day to you, {authorM}!',f'Konichi wa madda fakker!', f'You come here often?',f'I sincerely hope you don\'t come into any bodily harm {authorM}!','*ignores you*',f'Your name must be Bepis, cause you looking So-Da-licious:eyes:',f'Are you a keyboard {authorM}? Cause you\'re my type',f'Hey {authorM}! Did you know that blood can be a substitute for eggs! Just letting you know!']
@@ -45,14 +49,15 @@ class MyClient(discord.Client):
                         break
                 if perm == 'Mr Bot Maker Man':
                     await message.channel.send(f'GOT HERE {authorM} :eyes:')
+                    await message.channel.send(f'')
                     perm = ''
-                    #do the ting
+                    #do the ting TODO - figure out how to read mention in message, store ALL of the mentioned users permissions in an array, assign a jail perm, make unjail function
                 else:
                     await message.channel.send(f'Sorry {authorM}, you don\'t have permission to do that!\nTry asking the server admin for permission!')
             elif message.content.startswith('+sauce'):
                 await message.channel.send('Nothing yet. Check back soon...')
-            else:
-                await message.channel.send(f'Invalid command {authorM}! Try using "+c" for a list of commands')
+            #else: #TODO this breaks it into an infinite loop of reading its own messages as command attempts
+                #await message.channel.send(f'Invalid command {authorM}! Try using "+c" for a list of commands')
                 
 client = MyClient()
 client.run(TOKEN)
