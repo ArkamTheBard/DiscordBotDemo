c463389ce493a3723bd698ff4ccf27d4edb9a75e
Yardley Gutierrez <9100062+ArkamTheBard@users.noreply.github.com>
2020-09-05 01:08:35 -0500

    Updated bot.py
    Added beginning to jail command
    
    TODO: remove roles and store

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 #Define import statements
 import discord, os, requests, json
 from discord.ext import commands
+from discord.ext.commands import Bot
+from discord import message
 
 #Grabbing the environment variable value which is the token
 TOKEN = os.getenv('DISCORD')
@@ -19,6 +21,43 @@ async def greet(ctx):
 @client.command(aliases=['p'])
 async def ping(ctx):
     await ctx.send('Pong!')
+@client.command(aliases=['j'])
+async def jail(ctx, *, membername): #ctx, #member: discord.Member):
+    for role in ctx.message.author.roles:
+        if role.name == 'Mr Bot Maker Man':
+            #Grab the guild that we are trying to work with
+            guild = client.get_guild(ctx.message.guild.id)
+
+            #grab a list of all the users in the guild
+            member_list = guild.members
+
+            for member in member_list:
+                print(f'{member.nick} : {member.display_name}')
+
+            #voice channel object that we are moving the user to
+            other_voice_channel = guild.get_channel(397552560019341329)
+
+            #Grab the member we are moving
+            member_obj = ''
+
+            #Search the list of users and assign user to var if found else 
+            for member in member_list:
+                if member.nick == membername or member.display_name == membername:
+                    member_obj = guild.get_member(member.id)
+                    break
+                else:
+                    member_obj = None
+
+            if member_obj != None:
+                #voice channel to move user to
+                await member_obj.move_to(other_voice_channel)
+            else:
+                await ctx.send('Failed to find member')
+                    
+            
+
+
+    await ctx.send('Finished')
 @client.command(aliases=['a'])
 async def anime(ctx,*,anime):
     #Don't ask how this works haha
